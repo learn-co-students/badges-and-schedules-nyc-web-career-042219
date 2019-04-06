0c7d78ec1bca47dcc1a530a0171ecd235b786dc5
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-06 20:53:30 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 
-speakers = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+attendees = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 def badge_maker(array)
-  puts "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array)
