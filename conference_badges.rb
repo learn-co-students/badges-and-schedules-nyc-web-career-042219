f527dc6297476dc878615c59111bdee1445e5eb3
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-06 21:04:07 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,9 +5,18 @@ def badge_maker(array)
 end
 
 def batch_badge_creator(array)
-  
+  names_in_list = attendees.size 
+  names_taken_out = 0 
+    while names_taken_out < names_in_list
+    puts "Hello, my name is #{name}."
+    names_taken_out += 1
 end
 
 def assign_rooms(array, integer)
   
+end
+
+def printer
+  batch_badge_creator
+  assign_rooms
 end
