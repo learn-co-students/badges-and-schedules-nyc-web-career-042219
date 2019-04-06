07ee2f326804c8b44e844887c8c9de90e153c261
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-06 20:47:20 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+
+speakers = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+def badge_maker(array)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  
+end
+
+def assign_rooms(array, integer)
+  
+end
